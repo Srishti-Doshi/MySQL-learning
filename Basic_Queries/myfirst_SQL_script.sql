7d31327c4aca46eeff74514535e9b70b319ddb4a
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-10-02 19:45:16 +0530

    Add SQL script: CREATE DATABASE, USE, CREATE TABLE, SELECT, DROP DATABASE

---
+++ Basic_Queries/myfirst_SQL_script.sql
@@ -0,0 +1,21 @@
+-- Create a database
+CREATE DATABASE startersql;
+
+-- Set it as default schema
+USE startersql;
+
+-- Create a Table
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male', 'Female', 'Other'),
+    date_of_birth DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Select all columns -> this fetch every column and every row from the users table
+SELECT * FROM users;
+
+-- Drop the database -> delete the entire database(and all its tables)
+DROP DATABASE startersql;
